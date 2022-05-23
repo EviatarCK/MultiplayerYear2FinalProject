c25d8f69170316fd4954e43e8c6f44040cd85f2b
RoxasCodes <evicokazzz@gmail.com>
2022-05-23 20:36:55 +0300

    first push

---
+++ Assets/Scripts/Look.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Look : MonoBehaviour
+{
+    public static bool cursorLocked = true;
+
+    public Transform player;
+    public Transform cams;
+
+    public float xSensitivity;
+    public float ySensitivity;
+    public float maxAngle;
+
+    private Quaternion camCenter;
+
+    private void Start()
+    {
+        camCenter = cams.localRotation;
+    }
+
+    private void Update()
+    {
+        SetY();
+        SetX();
+        UpdateCursorLock();
+    }
+
+    void SetY()
+    {
+        float t_input = Input.GetAxis("Mouse Y") * ySensitivity * Time.deltaTime;
+        Quaternion t_adjust = Quaternion.AngleAxis(t_input, -Vector3.right);
+        Quaternion t_delta = cams.localRotation * t_adjust;
+
+        if (Quaternion.Angle(camCenter, t_delta) < maxAngle)
+        {
+            cams.localRotation = t_delta;
+        }
+
+        cams.localRotation = t_delta;
+    }
+    void SetX()
+    {
+        float t_input = Input.GetAxis("Mouse X") * xSensitivity * Time.deltaTime;
+        Quaternion t_adjust = Quaternion.AngleAxis(t_input, Vector3.up);
+        Quaternion t_delta = player.localRotation * t_adjust;
+        player.localRotation = t_delta;
+    }
+
+    void UpdateCursorLock()
+    {
+        if (cursorLocked)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                cursorLocked = false;
+            }
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                cursorLocked = true;
+            }
+        }
+    }
+}
