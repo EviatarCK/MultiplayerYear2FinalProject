88a4e22b26ebde367cd130dd131df16dcdf782d3
LiorAvitall <lior7112@gmail.com>
2022-07-18 15:22:58 +0300

    ADS

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -9,7 +9,7 @@ namespace Com.MultiPlayerProject
         #region Variables
         public Gun[] loadOut;
         public Transform weaponParent;
-
+        private int currentIndex;
         private GameObject currentWeapon;
         #endregion
 
@@ -21,6 +21,11 @@ namespace Com.MultiPlayerProject
             {
                 Equip(0);
             }
+
+            if (currentWeapon != null)
+            {
+                Aim(Input.GetMouseButton(1));
+            }
         }
         #endregion
 
@@ -31,12 +36,36 @@ namespace Com.MultiPlayerProject
             {
                 Destroy(currentWeapon);
             }
+
+            currentIndex = p_ind;
+
             GameObject t_newWeapon = Instantiate(loadOut[p_ind].prefab, weaponParent.position, weaponParent.rotation, weaponParent) as GameObject;
             t_newWeapon.transform.localPosition = Vector3.zero;
             t_newWeapon.transform.localEulerAngles = Vector3.zero;
 
             currentWeapon = t_newWeapon;
         }
+
+        void Aim(bool isAiming)
+        {
+            Transform t_anchor = currentWeapon.transform.Find("Anchor");
+            Transform t_states_ads = currentWeapon.transform.Find("States/ADS");
+            Transform t_states_hip = currentWeapon.transform.Find("States/Hip");
+
+            if (isAiming)
+            {
+                //aim
+                t_anchor.position = Vector3.Lerp(t_anchor.position, t_states_ads.position, Time.deltaTime * loadOut[currentIndex].aimSpeed);
+            }
+            else
+            {
+                //hip
+                t_anchor.position = Vector3.Lerp(t_anchor.position, t_states_hip.position, Time.deltaTime * loadOut[currentIndex].aimSpeed);
+
+            }
+        }
+
+
     #endregion
     }
 }
