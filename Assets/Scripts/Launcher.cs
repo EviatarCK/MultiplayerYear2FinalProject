ea5bed26a909fade5207aa9c8bbeff29c2fa2034
LiorAvitall <lior7112@gmail.com>
2022-07-18 17:19:53 +0300

    PUN2 setup

---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+namespace Com.MultiPlayerProject
+{
+    public class Launcher : MonoBehaviour
+    {
+        public void OnEnable()
+        {
+            PhotonNetwork.AutomaticallySyncScene = true;
+            Connect();
+        }
+
+        public void Connect()
+        {
+
+        }
+
+        public void Join()
+        {
+
+        }
+
+        public void StartGame()
+        {
+
+        }
+    }
+}
